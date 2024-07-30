671cc18874ef9bdb1cd6f25efc84254f6793951d
Musa Yiğit Yayla <yigit.yayla@ug.bilkent.edu.tr>
2024-07-30 13:46:26 +0300

    Schema refactored for automated table creation
    Now Kubernetes can create tables by reading the schema.sql specified in ConfigMap section of yaml (volume and volumeMounts sections of mysql-deployment.yaml).

--- schema.sql
+++ schema.sql
@@ -1,17 +1,17 @@
 CREATE TABLE IF NOT EXISTS user (
-    id LONG PRIMARY KEY AUTO_INCREMENT,
-    password TEXT NOT NULL
+                                    id BIGINT PRIMARY KEY AUTO_INCREMENT,
+                                    password TEXT NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS question (
-    id LONG PRIMARY KEY AUTO_INCREMENT,
-    ownerId LONG,
-    body TEXT NOT NULL,
-    answer0 TEXT,
-    answer1 TEXT,
-    answer2 TEXT,
-    answer3 TEXT,
-    answer_index INT,
-    FOREIGN KEY(ownerId) REFERENCES user(id),
+                                        id BIGINT PRIMARY KEY AUTO_INCREMENT,
+                                        ownerId BIGINT,
+                                        body TEXT NOT NULL,
+                                        answer0 TEXT,
+                                        answer1 TEXT,
+                                        answer2 TEXT,
+                                        answer3 TEXT,
+                                        answer_index INT,
+                                        FOREIGN KEY(ownerId) REFERENCES user(id),
     CHECK(answer_index < 4 AND answer_index >= 0)
-);
+    );
