31c14adb31198f167c60e02b2fab63748142e7da
Musa Yiğit Yayla <yigit.yayla@ug.bilkent.edu.tr>
2024-08-02 11:06:48 +0300

    Mid config push

--- src/main/java/com/example/quiz_service/config/DataConfig.java
+++ src/main/java/com/example/quiz_service/config/DataConfig.java
@@ -4,24 +4,43 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.context.ApplicationContext;
+import org.springframework.beans.factory.annotation.Autowired;
 
+import javax.annotation.PostConstruct;
 import javax.sql.DataSource;
 
 @Configuration
 public class DataConfig {
 
+    @Autowired
+    private ApplicationContext applicationContext;
     @Bean
     public DataSource dataSource(){
         DriverManagerDataSource dataSource = new DriverManagerDataSource();
         dataSource.setDriverClassName("com.mysql.cj.jdbc.Driver");
-        dataSource.setUrl("jdbc:mysql://localhost:3306/mydb");
+        dataSource.setUrl("jdbc:mysql://mysql-service:3307/mydb");
         dataSource.setUsername("user");
         dataSource.setPassword("password");
 
         return dataSource;
     }
+
     @Bean
     public JdbcTemplate jdbcTemplate(DataSource dataSource){
         return new JdbcTemplate(dataSource);
+    }
+
+    @PostConstruct
+    public void printDataSourceInfo() {
+        DataSource dataSource = (DataSource) applicationContext.getBean("dataSource");
+        if (dataSource instanceof DriverManagerDataSource) {
+            DriverManagerDataSource driverManagerDataSource = (DriverManagerDataSource) dataSource;
+            System.out.println("DataSource URL: " + driverManagerDataSource.getUrl());
+            System.out.println("DataSource Username: " + driverManagerDataSource.getUsername());
+        }
+        else {
+            System.out.println("DataSource is not of type DriverManagerDataSource");
+        }
     }
 }
