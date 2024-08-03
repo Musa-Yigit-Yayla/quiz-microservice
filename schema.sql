2e719f275d5358a4cec869d4277f65d22ddef8a3
Musa Yiğit Yayla <yigit.yayla@ug.bilkent.edu.tr>
2024-08-03 17:01:23 +0300

    Further implementation & db connection attempt

--- schema.sql
+++ schema.sql
@@ -1,11 +1,11 @@
 CREATE TABLE IF NOT EXISTS user (
-                                    id BIGINT PRIMARY KEY AUTO_INCREMENT,
+                                    id LONG PRIMARY KEY AUTO_INCREMENT,
                                     password TEXT NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS question (
-                                        id BIGINT PRIMARY KEY AUTO_INCREMENT,
-                                        ownerId BIGINT,
+                                        id LONG PRIMARY KEY AUTO_INCREMENT,
+                                        ownerId LONG,
                                         body TEXT NOT NULL,
                                         answer0 TEXT,
                                         answer1 TEXT,
@@ -15,3 +15,47 @@ CREATE TABLE IF NOT EXISTS question (
                                         FOREIGN KEY(ownerId) REFERENCES user(id),
     CHECK(answer_index < 4 AND answer_index >= 0)
     );
+
+CREATE TABLE IF NOT EXISTS test (
+                                    id LONG PRIMARY KEY AUTO_INCREMENT,
+                                    ownerId LONG,
+                                    name TEXT NOT NULL,
+                                    tag TEXT NOT NULL,
+                                    FOREIGN KEY (ownerId) REFERENCES user(id)
+);
+
+CREATE TABLE IF NOT EXISTS test_questions (
+    testId LONG,
+    questionId LONG,
+    PRIMARY KEY (testId, questionId),
+    FOREIGN KEY (testId) REFERENCES test(id),
+    FOREIGN KEY (questionId) REFERENCES user(id)
+);
+
+CREATE TABLE IF NOT EXISTS question_tags (
+    questionId LONG,
+    tag TEXT,
+    PRIMARY KEY (questionId, tag),
+    FOREIGN KEY(questionId) REFERENCES question(id)
+);
+
+CREATE TABLE IF NOT EXISTS test_add_request(
+    testId LONG,
+    questionId LONG,
+    requesterId LONG,
+    PRIMARY KEY (testId, questionId, requesterId),
+    FOREIGN KEY (testId) REFERENCES test(id),
+    FOREIGN KEY (questionId) REFERENCES question(id),
+    FOREIGN KEY (requesterId) REFERENCES user(id),
+    CHECK (requesterId <> (SELECT ownerId FROM test WHERE test.id = testId))
+);
+CREATE TABLE IF NOT EXISTS question_tag_request(
+    questionId LONG,
+    tag TEXT,
+    requesterId LONG,
+    PRIMARY KEY(questionId, requesterId, tag),
+    FOREIGN KEY(questionId) REFERENCES question(id),
+    FOREIGN KEY(requesterId) REFERENCES user(id),
+    CHECK (requesterId <> (SELECT ownerId FROM question WHERE question.id = questionId))
+);
+
