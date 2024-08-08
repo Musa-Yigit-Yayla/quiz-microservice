d2337b51f844b8b37140dfa70f0a9dbe53817e6e
Musa Yiğit Yayla <yigit.yayla@ug.bilkent.edu.tr>
2024-08-08 10:51:32 +0300

    HTTP success to kubernetes

--- src/main/java/com/example/quiz_service/QuizServiceApplication.java
+++ src/main/java/com/example/quiz_service/QuizServiceApplication.java
@@ -1,12 +1,17 @@
 package com.example.quiz_service;
 
+import com.example.quiz_service.repository.QuestionRepository;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @SpringBootApplication
 public class QuizServiceApplication {
 
 	public static void main(String[] args) {
+		Logger tempLogger = LoggerFactory.getLogger(QuizServiceApplication.class);
+		tempLogger.info("App.main scope is about to run the spring app");
 		SpringApplication.run(QuizServiceApplication.class, args);
 	}
 
